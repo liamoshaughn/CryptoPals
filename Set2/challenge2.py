1416912374701d8b44274bb7be0ea2aced4876bd
Liam O'Shaughnessy <liamoshaughn@gmail.com>
2025-07-11 16:19:20 +0930

    Added Set 2 Challenge 2 CBC Encryption

--- Set2/challenge2.py
+++ Set2/challenge2.py
@@ -1,5 +1,5 @@
-# AES-128 ECB Mode Decryption.
-# There is probably some unnecessary typing conversions in here but were used to help me debug with examples and have just stayed throughout
+# AES-128 CBC Mode Encryption.
+
 
 
 from Crypto.Util.Padding import pad
@@ -23,24 +23,7 @@ s_box = [
     [0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf],
     [0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16]
 ]
-inv_s_box = [
-    [0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb],
-    [0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb],
-    [0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e],
-    [0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25],
-    [0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92],
-    [0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84],
-    [0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06],
-    [0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b],
-    [0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73],
-    [0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e],
-    [0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b],
-    [0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4],
-    [0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f],
-    [0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef],
-    [0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61],
-    [0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d]
-]
+
 rounds = 11
 
 def gf_mult(a, b):
@@ -76,19 +59,19 @@ def state_to_hex(state_matrix):
             hex_str += hex_byte
     return hex_str
 
-def inverse_shift_rows(state_matrix):
+def shift_rows(state_matrix):
     for i in range(4):
-        #shift rows back to the right
-        state_matrix[i] = state_matrix[i][-i:] + state_matrix[i][:-i]
+        #shift rows to the left
+        state_matrix[i] = state_matrix[i][i:] + state_matrix[i][:i]
     
     return(state_matrix)
 
-def inverse_mix_columns(state_matrix):
+def mix_columns(state_matrix):
     inv_const_matrix = [
-    [0x0e, 0x0b, 0x0d, 0x09],
-    [0x09, 0x0e, 0x0b, 0x0d],
-    [0x0d, 0x09, 0x0e, 0x0b],
-    [0x0b, 0x0d, 0x09, 0x0e]
+    [0x02, 0x03, 0x01, 0x01],
+    [0x01, 0x02, 0x03, 0x01],
+    [0x01, 0x01, 0x02, 0x03],
+    [0x03, 0x01, 0x01, 0x02]
     ] 
     #Calculate a number using a polynomial formula with the variables being the above matrix and column first of the state matrix
     for i in range(4):
@@ -102,13 +85,13 @@ def inverse_mix_columns(state_matrix):
     
     return state_matrix
     
-def inverse_sub_bytes(state_matrix):
+def sub_bytes(state_matrix):
     for i in range(len(state_matrix)):
         for j in range(4):
             #split into nibbles so we can search s_box
             hex_part = state_matrix[i][j][2:].zfill(2)
             a, b = int(hex_part[0],16), int(hex_part[1],16) 
-            state_matrix[i][j] = hex(inv_s_box[a][b])
+            state_matrix[i][j] = hex(s_box[a][b])
     
     return state_matrix
     
@@ -153,11 +136,14 @@ def expand_key(key):
         keys[i] = ''.join(new_key)
         
     return keys
+
+def xor_strings(current, previous):
+    return bytes(a ^ b for a, b in zip(bytes.fromhex(current), bytes.fromhex(previous))).hex()
+    
             
 def main():
-    #For challenge 7 text is 7.txt and key is "YELLOW SUBMARINE" without quotes
 
-    file = input('Enter file to decrypt\n')
+    file = input('Enter file to encrypt\n')
 
     key = input('Enter key: ').encode('utf-8')
 
@@ -167,43 +153,48 @@ def main():
     elif len(key) > 16:
         key = key[:16]
         
-    print(key)
-
-    encrypted = ''
-
-    with open(file) as my_file:
-        encrypted += my_file.read()
-        
-    encrypted = base64.b64decode(encrypted)
-    if len(encrypted)%16 > 0:
-        encrypted = pad(encrypted, len(encrypted)%16)
-
-
-    chunks = [encrypted[i:i + 16] for i in range(0, len(encrypted), 16)]
-
-
-    keys = expand_key(key)
 
     decrypted = ''
+
+    with open(file) as my_file:
+        decrypted += my_file.read()
+    decrypted = decrypted.encode('utf-8')
+    if len(decrypted)%16 > 0:
+        decrypted = pad(decrypted, 16-len(decrypted)%16)
+    else:
+        decrypted = pad(decrypted, 16)
+    
+    chunks = [decrypted[i:i + 16] for i in range(0, len(decrypted), 16)]
+    keys = expand_key(key)
+
+    encrypted = ''
+    
+    #I'm just gonna call this iv but will be used to store the previous cipher block
+    iv = b'1234567812345678'.hex()
+    
     for chunk in chunks:
-        state_matrix = hex_to_state(chunk.hex())
+        #For CBC we have to XOR from previous Cipher
+        chunk = xor_strings(chunk.hex(), iv)
+        state_matrix = hex_to_state(chunk)
 
         #Every round but round 0 
-        for round in range(rounds-1, 0, -1):
-            state_matrix = add_round_key(keys[round], state_matrix)
+        state_matrix = add_round_key(keys[0], state_matrix)
+        for round in range(1,rounds, 1):
+            state_matrix = sub_bytes(state_matrix)
+            
+            state_matrix =  shift_rows(state_matrix)
             
             if round != 10:
-                state_matrix =  inverse_mix_columns(state_matrix) 
-                
-            state_matrix =  inverse_shift_rows(state_matrix)
-            state_matrix = inverse_sub_bytes(state_matrix)
-
+                state_matrix =  mix_columns(state_matrix) 
             
-        state_matrix = add_round_key(keys[0], state_matrix)
-        decrypted = decrypted + bytes.fromhex(state_to_hex(state_matrix)).decode('utf-8')
+            state_matrix = add_round_key(keys[round], state_matrix)
+    
+        iv = state_to_hex(state_matrix)
+       
+        encrypted += iv
 
 
-    print(decrypted)
+    print(encrypted)
 
 if __name__ == "__main__":
     main()
